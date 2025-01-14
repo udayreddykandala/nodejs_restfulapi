b36d21b603e976704a21fa11781ed0bc68e2cb22
udayreddykandala <136275899+udayreddykandala@users.noreply.github.com>
2025-01-14 17:50:07 +0000

    its a simple api

--- JOBBEE-API/app.js
+++ JOBBEE-API/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config/config.env" });
 //Setting up config.env file variables
+dotenv.config({ path: "./config/config.env" });
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`server is running on the port ${PORT}`);
+  console.log(
+    `server is running on the port ${PORT} in ${process.env.NODE_ENV} mode.`
+  );
 });
